8b3b13842be4cf684edaf2c01f133a17b15f7b3d
MartinaValkova <Martinavalkova16@hotmail.com>
2022-11-14 15:58:05 +0000

    test

--- src/ica_chatbot/core.clj
+++ src/ica_chatbot/core.clj
@@ -2,6 +2,7 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "CHATBOT: Hello, I'm a chatbot. What is your name?>")
+  (def   x (read-line))
+  (println (str "CHATBOT: Nice to meet you,"\space\  x "."))
