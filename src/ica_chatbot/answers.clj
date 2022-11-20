0931564e8bb7d4ddeae1ba1614f0bf5e9e79928d
pkrasnianska <polina.krasnianska@praguecollege.cz>
2022-11-20 13:01:47 +0100

    adds support for metro parsing, fixes transportation printin g

--- src/ica_chatbot/answers.clj
+++ src/ica_chatbot/answers.clj
@@ -13,16 +13,19 @@
     (get info-about-park request)))
 
 
-(defn print-transportation [means map]
-    (for [m map]
-    (for [[k v] m] (println (format "From station %s take %s: %s" k means v)))))
-
-
 (defn get-transportation [park]
   (let [t (get-park-info park :transportation)
-        trams (parser/parse-trams t)]
-        (print-transportation "tram" trams)))
-
+        trams (parser/parse-trams t)
+        metros (parser/parse-metro t)]
+        (if (not (nil? metros))
+            (do
+            (println "You can use the following metro stations/lines to get to the park: ")
+            (println metros)))
+        (if (not (nil? trams))
+            (do
+            (println "You can take the tram to the park:")
+            (for [m trams]
+              (for [[k v] m] (println (format "From station %s take lines: %s" k v))))))))
 
 ; (def transportations (for [[k v] parks-info] (:transportation v)))
 ;
--- src/ica_chatbot/parser.clj
+++ src/ica_chatbot/parser.clj
@@ -1,21 +1,6 @@
 (ns ica-chatbot.parser
   (:require [clojure.string :as str]))
 
-(defn match-regex [regex input]
-  (re-matches (re-pattern regex) input))()
-
-
-(defn get-intent [input]
-  (cond
-    (not (nil? (match-regex #".*(can|possible).*(bicycle|cycle|bike|ride a bike).*" input))) "biking"
-    (not (nil? (match-regex #".*(is|there|where|can|possible).*(toilet|wc|bathroom).*" input))) "wc"
-    :else :unknown))
-
-(defn parse-input []
-  (let [input (read-line)
-        intent (get-intent input)]
-  ))
-
 
 (defn parse-trams [t]
     (let [trams-str (re-find (re-pattern "(?:(?<=, )[^0-9]*|^(?:(?!Metro|metro|METRO|bus|Bus|BUS).)*)trams*[.]* +[Nn]o.[0-9, ]+") t)
@@ -24,3 +9,25 @@
                           {(first (str/split x #"\s*trams*[.]* +[Nn]o.\s*"))
                            (str/split (second (str/split x #"\s*trams*[.]* +[Nn]o.\s*")) #",\s*")}) trams-seq)]
           trams-map))
+
+
+(defn parse-metro [t]
+  (re-find (re-pattern "(?:.* - )*[Mm]etro .*?(?=$|, [A-Za-z]{2,})") t))
+
+
+  ; (defn match-regex [regex input]
+  ;   (re-matches (re-pattern regex) input))()
+  ;
+  ;
+  ; (defn get-intent [input]
+  ;   (cond
+  ;     (not (nil? (match-regex #".*(can|possible).*(bicycle|cycle|bike|ride a bike).*" input))) "biking"
+  ;     (not (nil? (match-regex #".*(is|there|where|can|possible).*(toilet|wc|bathroom).*" input))) "wc"
+  ;     :else :unknown))
+  ;
+  ;
+  ; (defn parse-input []
+  ;   (let [input (read-line)
+  ;         intent (get-intent input)]
+  ;   ))
+  ;
