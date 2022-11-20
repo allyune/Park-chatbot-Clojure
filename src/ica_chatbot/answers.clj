74d4ae43638f0ffa2da8a0220c9a4db72907b32c
pkrasnianska <polina.krasnianska@praguecollege.cz>
2022-11-20 07:59:38 +0100

    parses trams from specific park transportation

--- src/ica_chatbot/answers.clj
+++ src/ica_chatbot/answers.clj
@@ -3,8 +3,20 @@
   (:require [clojure.data.json :as json]))
 
   (def parks-json (io/resource "parks.json"))
-  (def parks-info (json/read-str (slurp parks-json)))
+  (def parks-info (json/read-json (slurp parks-json)))
 
   (defn get-park-info [park, request]
-    (let [info-about-park (get parks-info park)]
+    (let [info-about-park (get parks-info park)]s
     (get info-about-park request)))
+
+
+(defn parse-transportation [park]
+  (let [t (get-park-info park :transportation)
+        trams (re-find (re-pattern "(?:(?<=, )[^0-9]*|^(?:(?!Metro|metro|METRO|bus|Bus|BUS).)*)trams*[.]* +[Nn]o.[0-9, ]+") t)]
+        (println trams)))
+
+
+; (def transportations (for [[k v] parks-info] (:transportation v)))
+;
+;
+; (for [t transportations] (println (re-seq (re-pattern "(?:(?<=, )[^0-9]*|^(?:(?!Metro|metro|METRO|bus|Bus|BUS).)*)trams*[.]* +[Nn]o.[0-9, ]+") t)))
