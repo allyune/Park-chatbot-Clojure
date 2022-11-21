017a2b99165cb5acd5ae1cae16e5fdfbaab5d492
MARTINA VALKOVA <96818946+MartinaValkova@users.noreply.github.com>
2022-11-21 13:17:14 +0000

    Update core.clj
    Chatbot asking if the user need some information about Letna park

--- src/ica_chatbot/core.clj
+++ src/ica_chatbot/core.clj
@@ -1,7 +1,33 @@
 (ns ica-chatbot.core
   (:gen-class))
 
+(use 'clojure.java.browse)
+
+;;Positive responses
+(def Positive #{"Yes" "YES" "OK" "yes" "Yes please" "yes please"})
+
+
+;;Negative responses
+(def Negative #{"No" "Nope" "NO" "no" "NOPE"})
+
+;;Other responses
+(def Other #{"Not sure" "Maybe" "not sure"})
+
+
+(defn start
+      []
+      (println "Do you wish to get more information about Letná park in Prague?>")
+      (let [x (read-line)]
+            (cond
+                  (contains? Positive x) ()
+                  (contains? Negative x) "CHATBOT: I am here to help with information about parks in Prague!"
+                  (contains? Other x) "CHATBOT: Sounds like an issue"
+                  :else (start))))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+      [& args]
+      (println "CHATBOT: Hello, I'm a chatbot. What is your name?>")
+      (def   x (read-line))
+      (println (str "CHATBOT: Nice to meet you,"\space\  x "."))
+      (start))
