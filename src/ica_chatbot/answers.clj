1a4c214fd99fbffaf150a396f3a0eb367b2e084c
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-01-19 22:06:02 +0100

    fixes recommendation response text

--- src/ica_chatbot/answers.clj
+++ src/ica_chatbot/answers.clj
@@ -63,7 +63,7 @@
     (case response
       true (system/print-out (format "Yes %s is possible in %s" intent->phrase park-name))
       false (do (system/print-out (format "Unfortunately, you can't %s in %s." intent->phrase park-name))
-                (system/print-out (format "However, you can %s in the following parks: %s"
+                (system/print-out (format "However, %s is possible in the following parks: %s"
                                           intent->phrase (recommend-parks intent)))))))
 
 (defn print-attractions
